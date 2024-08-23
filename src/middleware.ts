9335fcca08c72ad98e8380fc24f090e7cb5f5726
Adarsh Sharma <124702646+adarshsharma7@users.noreply.github.com>
2024-08-23 14:27:22 +0530

    sa

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { getToken } from 'next-auth/jwt';
 export { default } from 'next-auth/middleware';
 
 export const config = {
-   matcher: ['/dashboard/:path*', '/sign-in', '/sign-up', '/', '/verify/:path*'],
+   matcher: ['/dashboard/:path*', '/sign-in', '/sign-up', '/', '/verify/:path*','/profile','/liked-videos','/subscriptionprofile/:path*','/videoplay/:path*','/watch-history','/watch-later','/subscriptions','/upload','/uploaded-videos'],
  
 };
 
@@ -18,12 +18,16 @@ export async function middleware(request: NextRequest) {
  
    // If the user is not authenticated and not on a public path, redirect to sign-in
    if (!token && !isPublicPath) {
+    if (url.pathname !== '/sign-in') {
      return NextResponse.redirect(new URL('/sign-in', request.url));
+    }
    }
  
    // If the user is authenticated and trying to access public paths, redirect to dashboard
    if (token && isPublicPath) {
+    if (url.pathname !== '/dashboard') {
      return NextResponse.redirect(new URL('/dashboard', request.url));
+    }
    }
 
   return NextResponse.next();
