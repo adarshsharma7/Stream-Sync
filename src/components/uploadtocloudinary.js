6af157d69c383a65ce7d909cbf20a6228da5921c
Adarsh Sharma <124702646+adarshsharma7@users.noreply.github.com>
2024-09-07 22:36:28 +0530

    Update uploadtocloudinary.js

--- src/components/uploadtocloudinary.js
+++ src/components/uploadtocloudinary.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-export const uploadToCloudinary = async (file, onProgress, resourceType = 'auto') => {
+export const uploadToCloudinary = async (file, onProgress,setLoading resourceType = 'auto') => {
     const formData = new FormData();
     formData.append("file", file);
     formData.append("upload_preset", "my_unsigned_preset"); // Replace with your unsigned upload preset
     // formData.append("cloud_name", `${process.env.CLOUD_NAME}`); // Replace with your Cloudinary cloud name
-//    setLoading(false)
+  setLoading(false)
 
     const response = await axios.post(
         `https://api.cloudinary.com/v1_1/dmep4qjdi/${resourceType}/upload`, // Replace with your Cloudinary API URL
