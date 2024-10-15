296e1086dd6c16cc268ee306463b16a610bedddb
Adarsh Sharma <124702646+adarshsharma7@users.noreply.github.com>
2024-10-15 20:54:50 +0530

    add forget password in profile chnage password

--- src/app/(app)/profile/page.jsx
+++ src/app/(app)/profile/page.jsx
@@ -26,6 +26,7 @@ function Page() {
     const [isSubmitting, setIsSubmitting] = useState(false);
     const [editProfileDialogOpen, setEditProfileDialogOpen] = useState(false);
     const [changePasswordDialogOpen, setChangePasswordDialogOpen] = useState(false);
+    const [forgetPassLoading, setForgetPassLoading] = useState(false);
 
 
     const popupRef = useRef(null);
@@ -171,7 +172,29 @@ function Page() {
 
     };
 
-
+    const forgetPassword=async()=>{
+        try {
+          setForgetPassLoading(true)
+          const response=await axios.post('api/users/forgetPassword',{credential:_user.email})
+          if(response.data.success){
+            toast({
+              title: 'Success',
+              description: response.data.message,
+         });
+          }
+          
+          if(response.data.message == 'Email is already sent for change password'){
+            toast({
+              title: 'Success',
+              description: response.data.message,
+         });
+          }
+        } catch (error) {
+          console.log("something wrong",error);
+        }finally{
+          setForgetPassLoading(false)
+        }
+      }
 
 
     return (
@@ -337,6 +360,13 @@ function Page() {
                                         className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                                     />
                                 </div>
+                                {forgetPassLoading ? (
+                                    <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                                ) : (
+                                    <button className='text-blue-700' type='button' onClick={forgetPassword}>Forget Password ?</button>
+                                )}
+
+
                                 <Button type="submit" className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700" disabled={isSubmitting}>
                                     {isSubmitting ? (
                                         <>
